d7e40adc64ffbb8d09136740e5debc8bfeff4099
Xeta <zoro.fmt@gmail.com>
2015-10-26 17:00:49 +0100

    Optimize the packet m + added cmd load/reload to load/reload a packet

--- lib/Rxb/packet.rb
+++ lib/Rxb/packet.rb
@@ -12,44 +12,98 @@ module Rxb
                 case packet.keys[0]
 
                 when "idle"
-                    Rxb::Kernel::reload(File.join(PACKETS, 'packets/idle.rb'))
+                    Rxb::Kernel::reload(File.join(PACKETS, 'idle.rb')) unless defined?(Rxb::Packets::Idle)
                     Rxb::Packets::Idle::onIdle(network)
 
                 when "z"
                     if packet['z']['@t'] == "/l"
-                        Rxb::Kernel::reload(File.join(PACKETS, 'z.rb'))
+                        Rxb::Kernel::reload(File.join(PACKETS, 'z.rb')) unless defined?(Rxb::Packets::Z)
                         Rxb::Packets::Z::onTickle(packet['z']['@u'], @config['bot']['id'])
                     end
 
                 when "m"
-                    if (packet['m'].has_key? "@u") && (packet['m'].has_key? "@i") && !(packet['m'].has_key? "@s") && !(packet['m'].has_key? "@p")
-                        cmdPrefix = packet['m']['@t'].split('')[0]
+                    message = {}
+                    message[:message] = packet['m']['@t']
 
-                        if cmdPrefix == "!"
-                            packet['m']['@t'] = packet['m']['@t'][1, packet['m']['@t'].length - 0]
+                    #Ignore all commands message starting with / (Like deleting a message, Typing etc).
+                    if !(message.has_key? :message) || (message[:message].split('')[0] == '/')
+                        return
+                    end
 
-                            message = packet['m']['@t'].split(' ')
+                    #Xat send sometimes the old messages, we ignore it so.
+                    if (packet['m'].has_key? "@s")
+                        return
+                    end
 
-                            case message[0]
+                    if !(packet['m'].has_key? "@i") || !(packet['m'].has_key? "@u") || (packet['m'].has_key? "@p")
+                        return
+                    end
 
-                                when "say"
-                                    message.delete('say')
-                                    message = message.join(" ").strip
 
-                                    if message.split('')[0] == "/"
-                                        Rxb::Xat.write_message("Nah.")
-                                    else
-                                        Rxb::Xat.write_message(message)
-                                    end
-                                when "info"
-                                    Rxb::Xat.write_message("Version : #{Rxb::Version::VERSION}  I'm coded in Ruby by Mars. Check how my body is made : https://github.com/Xety/Rxb :$")
-                                when "memory"
-                                    memory = `ps -o rss -p #{$$}`.strip.split.last.to_i / 1024
-                                    Rxb::Xat.write_message("Memory used (Standard CMD) : #{memory} Mb")
-                                else
-                                    Rxb::Xat.write_message("Unknown command: #{message[0]}")
+                    cmdPrefix = packet['m']['@t'].split('')[0]
+
+                    if cmdPrefix != "!"
+                        return
+                    end
+
+                    packet['m']['@t'] = packet['m']['@t'][1, packet['m']['@t'].length - 0]
+
+                    message = packet['m']['@t'].split(' ')
+
+                    case message[0]
+                        when "say"
+                            message.delete('say')
+                            message = message.join(" ").strip
+
+                            if message.split('')[0] == "/"
+                                Rxb::Xat.write_message("Nah.")
+                            else
+                                Rxb::Xat.write_message(message)
                             end
-                        end
+
+                        when "info"
+                            Rxb::Xat.write_message("Version : #{Rxb::Version::VERSION}  I'm coded in Ruby by Mars. Check how my body is made : https://github.com/Xety/Rxb :$")
+
+                        when "memory"
+                            memory = `ps -o rss -p #{$$}`.strip.split.last.to_i / 1024
+                            Rxb::Xat.write_message("Memory used (Standard CMD) : #{memory} Mb")
+
+                        when "reload"
+                            message.delete('reload')
+
+                            if message.any?
+                                if !File.exist?(PACKETS + "#{message[0]}.rb")
+                                    Rxb::Xat.write_message("The packet #{message[0]} doesn't exist.")
+                                    return
+                                end
+
+                                begin
+                                    Rxb::Kernel::reload(File.join(PACKETS, "#{message[0]}.rb"))
+                                    Rxb::Xat.write_message("The packet #{message[0]} has been reloaded successfully.")
+                                rescue
+                                    Rxb::Xat.write_message("Error to reload the packet #{message[0]}.")
+                                end
+                            end
+
+                        when "load"
+                            message.delete('load')
+
+                            if message.any?
+                                if !File.exist?(PACKETS + "#{message[0]}.rb")
+                                    Rxb::Xat.write_message("The packet #{message[0]} doesn't exist.")
+                                    return
+                                end
+
+                                if defined?(Object::const_get("Rxb::Packets::#{message[0].capitalize}"))
+                                    Rxb::Xat.write_message("The packet #{message[0]} has already been loaded.")
+                                else
+                                    Rxb::Kernel::reload(File.join(PACKETS, "#{message[0]}.rb"))
+                                    Rxb::Xat.write_message("The packet #{message[0]} has been loaded successfully.")
+                                end
+                            end
+
+                        else
+                            Rxb::Xat.write_message("Unknown command: #{message[0]}")
                     end
 
                 else
